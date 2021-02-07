a7943d69970d1a97c15d876144caaea10404fe1c
JaFr <jaromir.franek@ibillboard.com>
2021-02-07 10:08:01 +0100

    gatherer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 const {spawnerCore} = require('./spawn.js');
-
-const gatherers = _.filter(Game.creeps, (c, name) => name.match(/^bGather/));
+const gathererBrain = require('./role-harvester');
+const gatherers = getGatherers();
 
 if (gatherers.length < 3) {
     spawnerCore.spawnGatherer();
+}
+for (const gatherer of gatherers) {
+    gathererBrain.run(gatherer);
+}
+
+function getGatherers() {
+    return _.filter(Game.creeps, (c, name) => name.match(/^bGather/));
 }
