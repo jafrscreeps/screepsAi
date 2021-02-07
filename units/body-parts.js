0ce1d4b108d1f2b75db3fddc2b9c66f9c24a563c
JaFr <jaromir.franek@ibillboard.com>
2021-02-07 09:31:36 +0100

    initial

---
+++ units/body-parts.js
@@ -0,0 +1,10 @@
+exports.BodyParts = {
+    MOVE: 'move',
+    WORK: 'work',
+    CARRY: 'carry',
+    ATTACK: 'attack',
+    RANGED_ATTACK: 'ranged_attack',
+    TOUGH: 'tough',
+    HEAL: 'heal',
+    CLAIM: 'claim'
+}
